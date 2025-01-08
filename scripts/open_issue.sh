c5764600b38260e5c12cb6700344036a61c7213d
Caleb Schilly <cwschilly@gmail.com>
2025-01-08 12:43:25 -0500

    initial attempt

---
+++ scripts/open_issue.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CURRENT_DIR="$(dirname -- "$(realpath -- "$0")")"
+PARENT_DIR="$(dirname "$CURRENT_DIR")"
+WORKING_DIR="$PARENT_DIR/output"
+ORG=cws-test-org
+REPOSITORY=$1
+
+MISSING_WORKFLOWS=("test1" "test2" "test3")
+
+ISSUE_TITLE="[Workflows] Missing Workflow(s)"
+ISSUE_BODY="The following workflows are missing from the repository:\n\n"
+for w in "${MISSING_WORKFLOWS[@]}"; do
+    ISSUE_BODY+="  - $w\n"
+done
+
+gh issue create \
+    --repo "$ORG/$REPOSITORY" \
+    --title "$ISSUE_TITLE" \
+    --body "$ISSUE_BODY"
+
+echo "--------------------------------------------------";
